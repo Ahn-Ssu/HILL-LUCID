6e79cb75e6978a0da2f7bbf088ea58205aa408d4
Ahn-Ssu <21600402@handong.edu>
2021-07-14 10:10:32 +0900

    test

--- practice.py
+++ practice.py
@@ -6,14 +6,15 @@ from utils.gravityCentor import *
           
 
 devList = create_devices_with_tries()
+len(devList)
 myDev = devList[0]
 
-configure_some_nodes(myDev)
+# configure_some_nodes(myDev)
 
-while cv2.waitKey(33)<0:
-    img = read_imgData(myDev) 
-    COG_img = find_centerOfGravity(img)
-    cv2.imshow("np to", COG_img)
+# while cv2.waitKey(33)<0:
+#     img = read_imgData(myDev) 
+#     COG_img = find_centerOfGravity(img)
+#     cv2.imshow("np to", COG_img)
     
 
-destroy_deviceConnection(myDev)
+# destroy_deviceConnection(myDev)
